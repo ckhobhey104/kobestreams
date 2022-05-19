6566d268a0b9cd09145b5bf7e4432a617ad4ebbb
ckhobhey <kobinaafful27@gmail.com>
2022-05-19 17:11:24 +0000

    updated StreamShow

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useRef } from "react";
+import flv from "flv.js";
 import { useParams as param } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchStream } from "./../../actions/index";
@@ -6,6 +7,10 @@ import { fetchStream } from "./../../actions/index";
 const StreamShow = () => {
   const id = param().id;
   const dispatch = useDispatch();
+  const videoRef = useRef(null);
+
+  const stream = useSelector((state) => state.streams[id]);
+  console.log("Stream:", stream);
 
   const fetchDispatch = useCallback(
     () => dispatch(fetchStream(id)),
@@ -14,9 +19,14 @@ const StreamShow = () => {
 
   useEffect(() => {
     fetchDispatch();
-  }, [fetchDispatch]);
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
 
-  const stream = useSelector((state) => state.streams[id]);
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  }, [fetchDispatch, id]);
 
   const renderStream = () => {
     if (!stream) {
@@ -24,6 +34,8 @@ const StreamShow = () => {
     }
     return (
       <React.Fragment>
+        <video ref={videoRef} style={{ width: "100%" }} controls />
+
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </React.Fragment>
